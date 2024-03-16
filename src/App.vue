3a93325a6dc08e3dafcdde16b68a34a430d4d81c
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-03-16 12:52:06 +0100

    init repo

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>hello world</h1>
+  <router-view>
+  </router-view>
+</template>
+
+<script>
+ import {store} from './data/store.js';
+ import axios from 'axios';
+
+
+export default {
+  name:'App',
+  data () { 
+    return {
+      store,
+    }
+  },
+  methods: {
+    getAllPlayers(){
+      axios.get(store.apiUrl + '/players').then((res)=>{
+        console.log(res);
+      })
+    }
+  },
+  mounted(){
+    this.getAllPlayers();
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
